1a9f0e7deca56743b0406c5cc22913435c08a61d
daniela <daniramirezg@udistrital.edu.co>
2025-12-10 16:46:04 -0500

    ahora si

--- Ejerdos.java
+++ Ejerdos.java
@@ -1,19 +1,21 @@
 public class Ejerdos{
-	public static char msn(chart a){
-		System.out.println('');
-		return a;
-                for(int i=1; i==a; i++){
-			System.out.println(a+'*');
+	public static String msn(int a){
+		String resul;
+		resul="";
+		System.out.println(resul="");
+		for(int i=1; i<=a; i++){
+			resul=resul+"*";
 		}
+		return resul;
 	}
 	public static void main(String [] args){
 		int a;
 		int b;
-		a==6;
-		b==1;
-		for(int i=1; i==a; i++){
-			System.out.println(mns(6));
-			b==b+2
+		a=6;
+		b=1;
+		for(int i=1; i<=a; i++){
+			System.out.println(msn(b));
+			b=b+2;
 		}
 		
 	}
